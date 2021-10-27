d126abbc5108f2230d79da51aa0db4e392ca535b
kevinten10 <596823919@qq.com>
2021-10-27 09:04:44 +0800

    release: release v1.0.0.RELEASE version

--- capa-adapter/src/main/java/group/rxcloud/capa/addons/adapter/package-info.java
+++ capa-adapter/src/main/java/group/rxcloud/capa/addons/adapter/package-info.java
@@ -1,4 +1,4 @@
 /**
- * @apiNote https://github.com/spring-projects/spring-framework/tree/main/spring-core/src/main/java/org/springframework/util/concurrent
+ * https://github.com/spring-projects/spring-framework/tree/main/spring-core/src/main/java/org/springframework/util/concurrent
  */
 package group.rxcloud.capa.addons.adapter;
